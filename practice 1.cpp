225df884ec9ce4b7c7a9d92461b7f62c65a68b7b
DangarKrishna <162779251+DangarKrishna@users.noreply.github.com>
2024-04-09 22:11:04 +0530

    Pratical 1

---
+++ practice 1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+    int main()
+    {
+        int K;
+        cout << "enter a value of K";
+        cin >> K;
+        if(K%2==0)
+        {
+            cout << "enter number is no prime number";
+        }
+        else
+        {
+            cout << "enter number is prime number";
+        }
+
+    }
